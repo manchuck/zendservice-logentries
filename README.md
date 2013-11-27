b5b628cae87652d60df497b64285532cc59a8ca1
manchuck <chuck@manchuck.com>
2013-11-27 13:15:24 -0500

    initial Commit with some basic exceptions

---
+++ README.md
@@ -0,0 +1,11 @@
+ZendService\LogEntries\Api component
+================================
+
+Provides support for LogEntries api
+
+Install dependencies using:
+
+```
+curl -s https://getcomposer.org/installer | php
+php composer.phar install
+```
---
+++ library/ZendService/LogEntries/Exception/InvalidArgumentException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendService\LogEntries\Exception;
+
+/**
+ * Class InvalidArgumentException
+ *
+ * @package ZendService\LogEntries\Exception
+ * @author  Chuck "MANCHUCK" Reeves <chuck@manchuck.com>
+ */
+class InvalidArgumentException extends \InvalidArgumentException
+{
+
+}
