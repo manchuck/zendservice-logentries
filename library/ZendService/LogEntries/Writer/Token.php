f2c4555ed0c6297011c17defa5c2b7b4d298f755
manchuck <chuck@manchuck.com>
2013-11-27 15:59:56 -0500

    Added Token writer and test

---
+++ library/ZendService/LogEntries/Writer/Token.php
@@ -0,0 +1,179 @@
+<?php
+ /**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendService\LogEntries\Writer;
+
+use Zend\Log\Formatter\Simple as SimpleFormatter;
+use Zend\Log\Writer\AbstractWriter;
+use ZendService\LogEntries\Exception\RuntimeException;
+
+/**
+ * Class Token
+ *
+ * @package ZendService\LogEntries\Writer
+ * @author  Chuck "MANCHUCK" Reeves <chuck@manchuck.com>
+ */
+class Token extends AbstractWriter
+{
+    const LOG_ENTRIES_ADDRESS     = 'tcp://api.logentries.com';
+    const LOG_ENTRIES_TLS_ADDRESS = 'tls://api.logentries.com';
+    const LOG_ENTRIES_PORT        = 10000;
+    const LOG_ENTRIES_TLS_PORT    = 20000;
+
+    /**
+     * @var string
+     */
+    protected $token;
+
+    /**
+     * @var string
+     */
+    protected $logSeparator = "\n";
+
+    /**
+     * @var resource
+     */
+    protected $resource;
+
+    /**
+     * @var bool
+     */
+    protected $useTLS = false;
+
+    /**
+     * @var bool
+     */
+    protected $persistent = false;
+
+    /**
+     * @var int
+     */
+    protected $timeOut = 60;
+
+    /**
+     * @var int
+     */
+    protected $errorNum;
+
+    /**
+     * @var string
+     */
+    protected $errStr;
+
+    /**
+     * @param array|null|\Traversable $token
+     * @param bool $useTLS
+     * @param bool $persistent
+     * @param int $timeout
+     * @throws RuntimeException
+     */
+    public function __construct($token, $useTLS = false, $persistent = true, $timeout = 60)
+    {
+        if (empty($token)) {
+            throw new RuntimeException('You cannot set a token writer with an empty token');
+        }
+
+        $this->token      = (string) $token;
+        $this->useTLS     = (bool) $useTLS;
+        $this->persistent = (bool) $persistent;
+        $this->timeOut    = (float) $timeout;
+        $this->formatter  = new SimpleFormatter('%timestamp% - %priorityName% - %message% %extra%');
+    }
+
+    /**
+     * @return bool
+     */
+    public function isConnected()
+    {
+        return is_resource($this->resource) && !feof($this->resource);
+    }
+
+    protected function connect()
+    {
+        if ($this->isConnected()) {
+            return;
+        }
+
+        $port    = $this->useTLS ? self::LOG_ENTRIES_TLS_PORT : self::LOG_ENTRIES_PORT;
+        $address = $this->useTLS ? self::LOG_ENTRIES_TLS_ADDRESS : self::LOG_ENTRIES_ADDRESS;
+
+        if ($this->persistent) {
+            $resource = $this->openPersistentConnection($address, $port);
+        } else {
+            $resource = $this->openConnection($address, $port);
+        }
+
+        if (is_resource($resource) && !feof($resource)) {
+            $this->resource = $resource;
+        }
+    }
+
+    /**
+     * @param string $address
+     * @param int $port
+     * @return resource
+     * @cordCoverageIgnore
+     */
+    protected function openConnection($address, $port)
+    {
+        return @fsockopen($address, $port, $this->errorNum, $this->errStr, $this->timeOut);
+    }
+
+    /**
+     * @param string $address
+     * @param int $port
+     * @return resource
+     * @codeCoverageIgnore
+     */
+    protected function openPersistentConnection($address, $port)
+    {
+        return @pfsockopen($address, $port, $this->errorNum, $this->errStr, $this->timeOut);
+    }
+
+    /**
+     * @param $line
+     */
+    protected function writeToLogEntries($line)
+    {
+        if ($this->isConnected()) {
+            fputs($this->resource, $line);
+        }
+    }
+
+    /**
+     * @param string|\Zend\Log\Formatter\FormatterInterface $formatter
+     * @return $this|AbstractWriter
+     */
+    public function setFormatter($formatter)
+    {
+        return $this;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function shutdown()
+    {
+        if (is_resource($this->resource) && !$this->persistent) {
+            fclose($this->resource);
+            $this->resource = null;
+        }
+    }
+
+    /**
+     * @param array $event log data event
+     * @return void
+     */
+    protected function doWrite(array $event)
+    {
+        $this->connect();
+        $line = $this->token . ' ' . $this->formatter->format($event) . $this->logSeparator;
+        $this->writeToLogEntries($line);
+    }
+}
---
+++ tests/ZendService/LogEntries/Writer/TokenTest.php
@@ -0,0 +1,112 @@
+<?php
+ /**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendServiceTest\LogEntries\Writer;
+
+use \PHPUnit_Framework_TestCase;
+use Zend\Log\Logger;
+use ZendService\LogEntries\Writer\Token;
+
+/**
+ * Class TokenTest
+ *
+ * @package ZendServiceTest\LogEntries\Writer
+ * @author  Chuck "MANCHUCK" Reeves <chuck@manchuck.com>
+ */
+class TokenTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @param array $params
+     * @return \PHPUnit_Framework_MockObject_MockObject|\ZendService\LogEntries\Writer\Token
+     */
+    public function getTokenMock(array $params = array())
+    {
+        return $this->getMock(
+            '\ZendService\LogEntries\Writer\Token',
+            array('openConnection', 'openPersistentConnection', 'writeToLogEntries'),
+            $params
+        );
+    }
+
+    public function testLogsWarningWithNonPersistentTCPConnection()
+    {
+        $leToken = uniqid();
+        $token = $this->getTokenMock([$leToken, false, false]);
+        $token->expects($this->once())
+            ->method('openConnection')
+            ->with(Token::LOG_ENTRIES_ADDRESS, Token::LOG_ENTRIES_PORT);
+
+        $token->expects($this->once())
+            ->method('writeToLogEntries')
+            ->will($this->returnCallback(function ($value) use ($leToken) {
+                $this->assertRegExp('/' . $leToken . ' .* - WARN - Test/', $value);
+            }));
+
+        $logger = new Logger();
+        $logger->addWriter($token);
+        $logger->warn('Test');
+    }
+
+    public function testLogsWarningWithPersistentTCPConnection()
+    {
+        $leToken = uniqid();
+        $token = $this->getTokenMock([$leToken, false]);
+        $token->expects($this->once())
+            ->method('openPersistentConnection')
+            ->with(Token::LOG_ENTRIES_ADDRESS, Token::LOG_ENTRIES_PORT);
+
+        $token->expects($this->once())
+            ->method('writeToLogEntries')
+            ->will($this->returnCallback(function ($value) use ($leToken) {
+                $this->assertRegExp('/' . $leToken . ' .* - WARN - Test/', $value);
+            }));
+
+        $logger = new Logger();
+        $logger->addWriter($token);
+        $logger->warn('Test');
+    }
+
+    public function testLogsWarningWithNonPersistentTLSConnection()
+    {
+        $leToken = uniqid();
+        $token = $this->getTokenMock([$leToken, true, false]);
+        $token->expects($this->once())
+            ->method('openConnection')
+            ->with(Token::LOG_ENTRIES_TLS_ADDRESS, Token::LOG_ENTRIES_TLS_PORT);
+
+        $token->expects($this->once())
+            ->method('writeToLogEntries')
+            ->will($this->returnCallback(function ($value) use ($leToken) {
+                $this->assertRegExp('/' . $leToken . ' .* - WARN - Test/', $value);
+            }));
+
+        $logger = new Logger();
+        $logger->addWriter($token);
+        $logger->warn('Test');
+    }
+
+    public function testLogsWarningWithPersistentTLSConnection()
+    {
+        $leToken = uniqid();
+        $token = $this->getTokenMock([$leToken, true]);
+        $token->expects($this->once())
+            ->method('openPersistentConnection')
+            ->with(Token::LOG_ENTRIES_TLS_ADDRESS, Token::LOG_ENTRIES_TLS_PORT);
+
+        $token->expects($this->once())
+            ->method('writeToLogEntries')
+            ->will($this->returnCallback(function ($value) use ($leToken) {
+                $this->assertRegExp('/' . $leToken . ' .* - WARN - Test/', $value);
+            }));
+
+        $logger = new Logger();
+        $logger->addWriter($token);
+        $logger->warn('Test');
+    }
+}
